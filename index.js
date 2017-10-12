34bbb5f12e56ec4d6c6ea5e07efbfd27af4da491
Benjamin Rancourt <Benjamin.Rancourt@USherbrooke.ca>
2017-10-12 08:39:54 -0400

    Add 3 custom rules

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
 module.exports = {
   extends: ['standard'],
   parserOptions: {
-    ecmaVersion: 6
+    ecmaVersion: 6,
   },
   env: {
     browser: true,
     es6: true,
-    node: true
+    node: true,
   },
   plugins: [
     'html',
-    'json'
+    'json',
   ],
   rules: {
-    'new-cap': ['error', {
-      capIsNewExceptions: ['Polymer'],
-      capIsNewExceptionPattern: '^(UdeS|Polymer)'
-    }]
+    'comma-dangle': ['error', 'always-multiline'],
+    'require-jsdoc': [
+      'error', {
+        'require': {
+          'FunctionDeclaration': true,
+          'MethodDefinition': true,
+          'ClassDeclaration': true,
+        },
+      }],
+    'valid-jsdoc': 'error',
+    'new-cap': [
+      'error', {
+        capIsNewExceptions: ['Polymer'],
+        capIsNewExceptionPattern: '^(UdeS|Polymer)',
+      }],
   },
   globals: {
     UdeS: true,
-    Polymer: true
-  }
+    Polymer: true,
+  },
 }
